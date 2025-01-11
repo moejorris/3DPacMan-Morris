731dcb7cf03b138a94c66eb7ae45c20b8d466fad
moejorris <136379332+moejorris@users.noreply.github.com>
2025-01-11 01:12:14 -0500

    Pac-Man Style Movement Finished
    Turn Buffer Fully Functional (and hopefully bug proof!)
    
    Added Offscreen warping (going off screen on the right wraps you back around on the left side of the screen, vice a versa)
    
    Added FX

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,14 +1,14 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 4f;
     Rigidbody rb;
-    Vector3 curDir;
     Vector3 nextDir;
     Vector3 input;
 
-
+    [SerializeField] float checkBoxSize = 0.4f;
     Vector3 vel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -20,45 +20,52 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         InputCheck();
-        // UpdateIntendedDirection();
-
-        if(input.x != 0)
-        {
-            vel.x = Mathf.Sign(input.x);
-        }
-        if(input.y != 0)
-        {
-            vel.z = Mathf.Sign(input.y);
-        }
-
+        UpdateIntendedDirection();
+        CheckForWarp();
         rb.linearVelocity = vel * moveSpeed;
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, curDir);
+        Gizmos.DrawRay(transform.position, vel);
 
         Gizmos.color = Color.blue;
-        Gizmos.DrawRay(transform.position, nextDir);
+        Gizmos.DrawWireCube(transform.position + nextDir, transform.localScale * (checkBoxSize * 2f));
+        // Gizmos.DrawRay(transform.position, transform.right * 2f);
     }
 
 
 
     void InputCheck()
     {
-        input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
 
     void UpdateIntendedDirection()
     {
-        if(input != Vector3.zero)
+        if(input.x != 0 ^ input.z != 0)
         {
-            nextDir = input;
+            nextDir = input.normalized;
         }
-        if(nextDir == curDir)
+        
+        if(nextDir == Vector3.zero) return;
+
+        if(!Physics.CheckBox(transform.position + nextDir, transform.localScale * checkBoxSize) && !Physics.Raycast(transform.position, nextDir, 0.6f) && nextDir != Vector3.zero)
         {
+            Debug.Log("new dir assigned");
+            vel = nextDir;
             nextDir = Vector3.zero;
+        }
+    }
+
+    void CheckForWarp()
+    {
+        float xPos = transform.position.x;
+
+        if(Mathf.Abs(xPos) > 17.5 && Mathf.Sign(xPos) == Mathf.Sign(vel.x))
+        {
+            transform.position = new Vector3(-xPos, 0, 0.5f);
         }
     }
 }
